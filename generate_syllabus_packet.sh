d88c189011ddde3c1aea3bc96af1ab7742bb96f8
Michael Weylandt <michael.weylandt@baruch.cuny.edu>
2025-01-22 17:11:34 -0500

    Update syllabus packet script

--- generate_syllabus_packet.sh
+++ generate_syllabus_packet.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 COURSE=STA9890
+CHROME="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+BASE_URL='https://michael-weylandt.com/'
+
+as_pdf () {
+  "$CHROME" --headless --disable-gpu --print-to-pdf=$1.pdf $BASE_URL/$COURSE/$1.html
+}
+
+
+declare -a PAGES=("syllabus" "resources" "objectives" "reports")
+PDFS=("${PAGES[@]}")
 
 mkdir _tmp_syllabuspacket
 cd _tmp_syllabuspacket
-wkhtmltopdf https://michael-weylandt.com/$COURSE/syllabus.html   syllabus.pdf
-wkhtmltopdf https://michael-weylandt.com/$COURSE/resources.html  resources.pdf
-wkhtmltopdf https://michael-weylandt.com/$COURSE/objectives.html objectives.pdf
 
-pdftk syllabus.pdf resources.pdf objectives.pdf cat output "$COURSE"_syllabus_packet.pdf
+for page in "${PAGES[@]}"
+do
+  echo "$page"
+  as_pdf "$page"
+done
+
+for i in "${!PDFS[@]}"
+do
+  PDFS[$i]="${PDFS[$i]}.pdf"
+done
+
+pdftk "${PDFS[@]}" output "$COURSE"_syllabus_packet.pdf
+
 mv "$COURSE"_syllabus_packet.pdf ..
 cd ..
 rm -rf _tmp_syllabuspacket
